cfcdc5b07db674a755ab0f54ab69e5c39e34d9db
Areeb Alam <areeb@Areebs-MacBook-Air.local>
2025-02-18 19:56:23 -0500

    added 5 new functions which include talking, eating, sleeping, moving, and playing

--- Animal.py
+++ Animal.py
@@ -1,7 +1,23 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name):
+        self.__name = name
+        print("Hello, I am", self.__name)
 
     def talk(self):
-        print("hi")
+        print("Hi")
 
+    def eat(self, food):
+        print(self.__name, "is eating", food)
+
+    def sleep(self):
+        print(self.__name, "is sleeping")
+
+    def move(self):
+        print(self.__name, "is moving around")
+
+    def play(self):
+        print(self.__name, "is playing")
+
+
+
+   
